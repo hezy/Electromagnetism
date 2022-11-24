5efeea54230998aa10a10c69ed25b9735ffd691f
Hezy Amiel <hezy.amiel@gmail.com>
2022-11-25 00:44:30 +0200

    electric dipole

--- electric_dipole.py
+++ electric_dipole.py
@@ -14,9 +14,9 @@ x, y = np.meshgrid(sx,sy)
 
 k = 9E9
 q1 = 1E-9
-x1, y1  = -0.317, 0
+x1, y1  = -0.3, 0
 q2 = -1E-9
-x2, y2  = 0.317, 0
+x2, y2  = 0.3, 0
 
 
 V = (k*q1/np.sqrt((x-x1)**2+(y-y1)**2) + 
@@ -32,6 +32,7 @@ plt.show()
 
 Ex = (k*q1*(x-x1)/((x-x1)**2+(y-y1)**2)**(3/2) +
       k*q2*(x-x2)/((x-x2)**2+(y-y2)**2)**(3/2))
+
 Ey = (k*q1*(y-y1)/((x-x1)**2+(y-y1)**2)**(3/2) +
       k*q2*(y-y2)/((x-x2)**2+(y-y2)**2)**(3/2))
 
@@ -43,8 +44,8 @@ E_max = 400
 E[E>E_max] = E_max
 E[E<-E_max] = -E_max
 
-#plt.quiver(x, y, cos, sin, E, pivot='mid')
-plt.streamplot(x, y, Ex, Ey)
+plt.quiver(x, y, cos, sin, E, pivot='mid')
+#plt.streamplot(x, y, Ex, Ey)
 plt.colorbar(label='Electrical Field [$V/m$]')
 plt.title('Electrical Field of a Dipole')
 plt.xlabel('Horizontal Position [m]')
@@ -54,7 +55,7 @@ plt.show()
 dx = x[0,1] - x[0,0]
 dy = y[1,0] - y[0,0]
 
-Ex_, Ey_ = np.gradient(V, dx, dy).T
+Ex_, Ey_ = np.gradient(V, dx, dy)
 Ex_ = -1 * Ex_
 Ey_ = -1 * Ey_
 
@@ -67,8 +68,8 @@ E_max_ = 400
 E_[E_>E_max_] = E_max_
 E_[E_<-E_max_] = -E_max_
 
-#plt.quiver(x, y, cos_, sin_, E_, pivot='mid')
-plt.streamplot(x, y, Ex_, Ey_)
+plt.quiver(x, y, cos_, sin_, E_, pivot='mid')
+#plt.streamplot(x, y, Ex_, Ey_)
 plt.colorbar(label='Electrical Field [$V/m$]')
 plt.title('Electrical Field of a Dipole')
 plt.xlabel('Horizontal Position [m]')
